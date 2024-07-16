995232dff21902ed6ebf6a8ed6c3a42998eb0436
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-07-16 15:46:21 +0300

    feat: add EmployeeService methods

--- src/main/java/by/ustsinovich/testcase/service/EmployeeService.java
+++ src/main/java/by/ustsinovich/testcase/service/EmployeeService.java
@@ -1,4 +1,19 @@
 package by.ustsinovich.testcase.service;
 
+import by.ustsinovich.testcase.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    Employee getEmployeeById(Long id);
+
+    Employee createEmployee(Employee employee);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+    void deleteEmployee(Long id);
+
 }
