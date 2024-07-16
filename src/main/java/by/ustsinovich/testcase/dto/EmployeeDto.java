b2cc5b5b1a2e2634f592f04eafca18ddbacde016
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-07-16 18:33:15 +0300

    feat: add EmployeeDto

---
+++ src/main/java/by/ustsinovich/testcase/dto/EmployeeDto.java
@@ -0,0 +1,75 @@
+package by.ustsinovich.testcase.dto;
+
+public class EmployeeDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String patronymic;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    private String jobTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+    
+}
