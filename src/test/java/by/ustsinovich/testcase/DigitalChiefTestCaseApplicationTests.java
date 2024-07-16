daea448692de2eb3d90220ab822d1dabb63b9178
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-07-16 07:31:38 +0300

    chore: initialize project

---
+++ src/test/java/by/ustsinovich/testcase/DigitalChiefTestCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package by.ustsinovich.testcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefTestCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
