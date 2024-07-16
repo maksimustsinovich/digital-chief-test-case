864d9c5d96f80064958bdc8b95f82d97c833b10f
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-07-16 16:24:57 +0300

    feat: add DepartmentService methods

--- src/main/java/by/ustsinovich/testcase/service/DepartmentService.java
+++ src/main/java/by/ustsinovich/testcase/service/DepartmentService.java
@@ -1,4 +1,26 @@
 package by.ustsinovich.testcase.service;
 
+import by.ustsinovich.testcase.entity.Department;
+import by.ustsinovich.testcase.entity.Employee;
+
+import java.util.List;
+
 public interface DepartmentService {
+
+    List<Department> getAllDepartments();
+
+    Department getDepartmentById(Long id);
+
+    Department createDepartment(Department department);
+
+    Department updateDepartment(Long id, Department newDepartment);
+
+    void deleteDepartment(Long id);
+
+    List<Employee> getEmployeesByDepartment(Long departmentId);
+
+    void addEmployeeToDepartment(Long departmentId);
+
+    void removeEmployeeFromDepartment(Long departmentId, Long employeeId);
+
 }
